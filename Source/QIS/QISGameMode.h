b480a76ecdd948f8c9db74a6cd1529f5f14dfea0
Rob <baldy.meade@gmail.com>
2023-04-13 13:53:57 +0100

    Initial Project Commit

---
+++ Source/QIS/QISGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QISGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AQISGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AQISGameMode();
+};
+
+
+
